767d9101a5a473fdcade3288394254fc8002cd28
MichaBrugger <michabrugger14@gmail.com>
2021-07-08 00:15:34 +0200

    changing theme colors

--- src/components/landing.js
+++ src/components/landing.js
@@ -3,13 +3,13 @@ import "../styles/layout.css";
 
 const Landing = props => {
   return (
-    <div className="content">
+    <header className="content">
       <h1>
         t<span class="highlight">h</span>e world's lam
         <span class="highlight">e</span>st&nbsp;<span class="highlight">l</span>
         anding&nbsp;<span class="highlight">p</span>age.
       </h1>
-    </div>
+    </header>
   );
 };
 
