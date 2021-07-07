b1fa26f23fb27d67e4b8ebe4fe3c229cc3257e61
MichaBrugger <michabrugger14@gmail.com>
2021-07-08 00:43:28 +0200

    changing theme colors

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import style from '../styles/icon.module.css'
+import style from "../styles/icon.module.css";
 
 const Icon = props => {
-  const { d, size = '1em', label, style: styles } = props
+  const { d, size = "1em", label, style: styles } = props;
 
   return (
-    <span className={style.root} style={styles} role="figure">
+    <span className={style.root} style={styles} role="button">
       <svg
         version="1.1"
         width={size}
@@ -19,14 +19,14 @@ const Icon = props => {
       </svg>
       {label && <span className={style.label}>{label}</span>}
     </span>
-  )
-}
+  );
+};
 
 Icon.propTypes = {
   d: PropTypes.string,
   size: PropTypes.number,
   label: PropTypes.string,
   style: PropTypes.object,
-}
+};
 
-export default Icon
+export default Icon;
